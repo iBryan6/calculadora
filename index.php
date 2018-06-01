3f27353718ecbecf3b0ecccaa765b8a64951c72e
Bryan Argandoña <bryan.argandona@avantica.net>
2018-06-01 11:07:41 -0400

    calcu code

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+class calculadora{
+    public function sumar($a,$b){
+        echo ("La suma de $a y $b = "), $a+$b,"</br>";
+    }
+    public function resta($a,$b){
+        echo ("La resta de $a y $b = "), $a-$b,"</br>";
+    }
+    public function division($a,$b){
+        echo ("La division de $a y $b = "), $a/$b,"</br>";
+    }
+    public function multiplicacion($a,$b){
+        echo ("La multiplicacion de $a y $b = "), $a*$b,"</br>";
+    }
+}
+
+$calculadora = new calculadora();
+$calculadora->sumar(2,3);
+$calculadora->resta(2,3);
+$calculadora->division(2,3);
+$calculadora->multiplicacion(2,3);
+
+?>
