783c64b410220219b922f5bcdfe830c60d360cfb
Bryan Argandoña <bryan.argandona@avantica.net>
2018-06-07 12:00:32 -0400

    final fix

--- Calculadoratest.php
+++ Calculadoratest.php
@@ -1,13 +1,29 @@
 <?php
 
-use Calculadora;
+require 'Calculadora.php';
 use PHPUnit\Framework\TestCase;
 
-final class CalculadoraTest extends TestCase{
-    public function Tests(){
-        $this->assertsEquals(
-            5,
-            $calculadora->sumar(2,3)
-        );
+final class CalculadoraTests extends TestCase{
+    private $calculadora;
+
+    protected function setUp()
+    {
+        $this->calculadora = new Calculadora();
+    }
+
+    protected function tearDown()
+    {
+        $this->calculadora = NULL;
+    }
+
+    public function testSuma()
+    {
+        $result = $this->calculadora->sumar(1, 2);
+        $this->assertEquals(3, $result);
     }
 }?>
+
+    <!--        LINECODE BASH: vendor/bin/phpunit Calculadoratest.php
+        composer require --dev phpunit/phpunit ^7
+        composer dump-autoload -o
+        composer update-->
